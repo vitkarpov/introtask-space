43963687b0c09446204a752877992bb5463d3b45
Vit Karpov <viktor.s.karpov@gmail.com>
2013-08-01 20:36:41 +0400

    adds reports tests

--- _tests/tests.js
+++ _tests/tests.js
@@ -46,3 +46,22 @@ test("Загрузка корабля", function() {
 	equal(vessel.cargo, capacity, "Корабль полностью загружен");
 	equal(planetB.availableAmountOfCargo, availableAmountOfCargo - capacity, "Корабль выгрузил с планеты " + capacity + "т. груза");
 });
+
+var vessel = new Vessel('Яндекс', [0,0], 1000);
+var planetA = new Planet('A', [0,0], 0);
+var planetB = new Planet('B', [100, 100], 5000);
+
+// Проверка текущего состояния
+vessel.report(); // Корабль "Яндекс". Местоположение: 0,0. Занято: 0 из 1000т.
+planetA.report(); // Планета "A". Местоположение: 0,0. Грузов нет.
+planetB.report(); // Планета "B". Местоположение: 100,100. Доступно груза: 5000т.
+
+vessel.flyTo(planetB);
+planetB.loadCargoTo(vessel, 1000);
+vessel.report(); // Корабль "Яндекс". Местоположение: 100,100. Занято: 1000 из 1000т.
+
+vessel.flyTo(planetA);
+planetA.unloadCargoFrom(vessel, 500);
+vessel.report(); // Корабль "Яндекс". Местоположение: 0,0. Занято: 500 из 1000т.
+planetA.report(); // Планета "A". Местоположение: 0,0. Доступно груза: 500т.
+planetB.report(); // Планета "B". Местоположение: 100,100. Доступно груза: 4000т.
